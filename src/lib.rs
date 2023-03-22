98bb07c7fb7918c69bbeada81f217241549e7e8b
patrickmilnes <pjamilnes@gmail.com>
2023-03-22 19:30:16 +0000

    feat: Added structs and impl new methods

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,59 @@
+pub struct XmlDocument<'a> {
+    pub path: &'a str,
+    pub root: Option<Box<&'a XmlNode<'a>>>,
+}
+
+pub struct XmlNode<'a> {
+    pub tag: &'a str,
+    pub value: Option<&'a str>,
+    pub parent: Option<&'a XmlNode<'a>>,
+    pub children: Vec<&'a XmlNode<'a>>,
+}
+
+impl<'a> XmlDocument<'a> {
+    pub fn new(path: &'a str) -> Self {
+        XmlDocument {
+            path,
+            root: Option::None,
+        }
+    }
+}
+
+impl<'a> XmlNode<'a> {
+    pub fn new(
+        parent: Option<&'a XmlNode<'a>>,
+        tag: &'a str,
+        value: Option<&'a str>,
+        children: Vec<&'a XmlNode<'a>>,
+    ) -> Self {
+        XmlNode {
+            parent,
+            tag,
+            value,
+            children,
+        }
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use crate::{XmlDocument, XmlNode};
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn create_xml_document() {
+        let path = "./form.xml";
+        let doc = XmlDocument::new(path);
+        assert_eq!(doc.path, "./form.xml");
+    }
+
+    #[test]
+    fn create_xml_node() {
+        let node = XmlNode::new(Option::None, "root", Option::None, Vec::new());
+        assert_eq!(node.tag, "root");
+        assert_eq!(node.value, Option::None);
     }
 }
